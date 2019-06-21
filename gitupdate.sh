543835f97f14f1c95afe7887071e48f8a9de997f
nam2297ptit <namunited1234@gmail.com>
2019-06-21 17:15:49 +0700

    nam997ptit

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 cd Documents/
-
-
 git add .
 git status
 echo -n "Enter the Description for the Change: " [Minor Update]
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
-git push origin master
+git push -f  origin master
 
 exit
 
